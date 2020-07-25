09cd79908651a5d6180b602954ad68f883c55b10
David Taylor <dave@imagetaylor.com>
2020-07-26 08:21:50 +1000

    setup separate agent

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,24 @@
 Vagrant.configure("2") do |config|
   config.vm.define "sensuapp" do |sensuapp|
-    config.vm.box = "centos/8"
-    config.vm.box_version = "1905.1"
+    sensuapp.vm.box = "centos/8"
+	sensuapp.vm.provider "virtualbox" do |vb|
+		vb.memory = 2048
+		vb.cpus = 2
+	end
+	sensuapp.vm.network "private_network", ip: "10.0.0.17", netmask:"255.255.255.0"
 	sensuapp.vm.network "forwarded_port", guest: 3000, host: 80
+	sensuapp.vm.provision "shell", path: "https://github.com/davetayl/Vagrant-General/raw/master/setup-centos8.sh"
 	sensuapp.vm.provision "shell", path: "./provision-sensuapp.sh"
   end
+  config.vm.define "agent" do |agent|
+    agent.vm.box = "centos/8"
+	agent.vm.provider "virtualbox" do |vb|
+		vb.memory = 1024
+		vb.cpus = 1
+	end
+	agent.vm.network "private_network", ip: "10.0.0.18", netmask:"255.255.255.0"
+	agent.vm.provision "shell", path: "https://github.com/davetayl/Vagrant-General/raw/master/setup-centos7.sh"
+	agent.vm.provision "shell", path: "./provision-agent.sh"
+  end
+
 end
--- provision-sensuapp.sh
+++ provision-sensuapp.sh
@@ -6,39 +6,23 @@ echo ""; echo "-----------------------------------------"
 
 # Configure variables
 MYHOST=sensuapp
-TESTPOINT=google.com
 echo "- Variables set -"
-
-# Test internet connectivity
-ping -q -c5 $TESTPOINT > /dev/null 2>&1
- 
-if [ $? -eq 0 ]
-then
-	echo "- Internet Ok -"	
-else
-	echo "- Internet failed -"
-fi
 
 # Set system name
 hostnamectl set-hostname $MYHOST
 cat >> /etc/hosts <<EOF
-10.0.2.15	$MYHOST $MYHOST.localdomain
+10.0.0.17	$MYHOST $MYHOST.localdomain
 EOF
 echo "- Name set -"
 
-# Base OS update
-dnf -y update > /dev/null 2>&1
-echo "- OS Updated -"
-
-
 # Install tools
-dnf -y install net-tools tmux > /dev/null 2>&1
+dnf -yqe 3 install net-tools 
 echo "- Tools installed -"
 
 # Install Application
-curl -s https://packagecloud.io/install/repositories/sensu/stable/script.rpm.sh | sudo bash > /dev/null 2>&1
-dnf -y install sensu-go-agent sensu-go-cli sensu-go-backend > /dev/null 2>&1
-curl -L https://docs.sensu.io/sensu-go/latest/files/backend.yml -o /etc/sensu/backend.yml > /dev/null 2>&1
+curl -s https://packagecloud.io/install/repositories/sensu/stable/script.rpm.sh | sudo bash #> /dev/null 2>&1
+dnf -yqe 3 install sensu-go-agent sensu-go-cli sensu-go-backend #> /dev/null 2>&1
+curl -L https://docs.sensu.io/sensu-go/latest/files/backend.yml -o /etc/sensu/backend.yml #> /dev/null 2>&1
 curl -L https://github.com/davetayl/vagrant_sensu/blob/master/agent.yml -o /etc/sensu/agent.yml
 echo "- Sensu installed -"
 
